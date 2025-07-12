4152da3c72f368b2bf3b9c29b3bb8980311b8925
Farzan <farzanhasan85@gmail.com>
2025-07-12 19:18:57 +0530

    Move app.js to api/index.js for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const serverless = require("serverless-http");  // <== Wrap Express for Vercel
-const date = require("./date");
+const date = require("../date"); 
+
 
 const app = express();
-=======
-const express=require("express");
 const bodyparser=require("body-parser");
-const date=require(__dirname + "/date.js");
->>>>>>> f3912fd3610263033ad25b7d03e726892a9d909e
+
 
 // Store to-do items
 let itemarray = ["Buy Food", "Cook Food", "Eat Food"];
@@ -18,11 +16,11 @@ let workitem = [];
 
 // View and static setup
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));         // updated path
-app.use(express.static(path.join(__dirname, "public"))); // updated path
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
 // Routes
 app.get("/", function (req, res) {
   let day = date.getdate();
@@ -41,7 +39,7 @@ app.post("/", function (req, res) {
     itemarray.push(item);
     res.redirect("/");
   }
-=======
+});
 app.set('view engine', 'ejs');
 
 app.use(bodyparser.urlencoded({extended:true}));
@@ -67,7 +65,7 @@ app.post("/",function(req,res){
         itemarray.push(item);
         res.redirect("/");
     }
->>>>>>> f3912fd3610263033ad25b7d03e726892a9d909e
+
 });
 
 app.get("/work", function (req, res) {
@@ -81,14 +79,14 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-<<<<<<< HEAD
+
 // ⛔ REMOVE app.listen() – not allowed in Vercel
 
 // ✅ EXPORT handler for Vercel
 module.exports = app;
 module.exports.handler = serverless(app);
-=======
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
->>>>>>> f3912fd3610263033ad25b7d03e726892a9d909e
+
